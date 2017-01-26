95c82f1826b20b5759c17610c6f44b6ba6f14664
UNBREAKING <apaseishvili@gmail.com>
2017-01-27 02:47:29 +0300

    first try

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,13 +1,32 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this._length=0;
+        this.Head=null;
+        this.Tail=null;
+    }
 
-    append(data) {}
+    append(data) {
+            const elem = new Node(data, null, null);
+            if(this._length===0){
+                this.Head=elem;
+                this.Tail=elem;
+            }else{
+                this.Tail.next=elem;
+                elem.prev=this.Tail;
+                this.Tail=elem;
+            }
+            this._length++;
+    }
 
-    head() {}
+    head() {
+        return this.Head.data;
+    }
 
-    tail() {}
+    tail() {
+        return this.Tail.data;
+    }
 
     at(index) {}
 
